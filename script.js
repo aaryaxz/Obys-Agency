85e8b88263dce6c8380e639630f5750a15085319
Aarya <genzaarya@gmail.com>
2024-11-06 13:23:46 +0530

    feat: add preloader

---
+++ script.js
@@ -0,0 +1,37 @@
+var tl = gsap.timeline()
+tl.from(".lines h1,h2",  {
+    y: 100,
+    duration: 0.5,
+    stagger: 0.2,
+})
+
+
+tl.from(".counter",{
+    opacity: 0,
+    duration: 2,
+    onStart:function(){
+        var counterNumber1 = document.querySelector(".counter-number1");
+        var grow = 0
+        var int = setInterval(() => {
+            grow >= 100 && clearInterval(int)
+            counterNumber1.innerHTML = grow++;
+        }, 35)
+    }
+},"=-2")
+
+
+tl.from(".preloaderPara",{
+    opacity:0,
+    duration:.5,
+})
+
+tl.to(".preloader-content",{
+    opacity:0,
+    delay:2,
+    duration:1.7,
+})
+tl.to(".preloader",{
+    yPercent:-100,
+    delay:0.5,
+    ease:"sin.out"
+})
